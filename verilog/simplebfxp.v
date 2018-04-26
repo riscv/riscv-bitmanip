3cba1d9127bb5db64cbaaf4c8e7d23ef75aa3ef2
Clifford Wolf <clifford@clifford.at>
2018-04-26 13:55:40 +0200

    Improve simplebfxp reference core
    Signed-off-by: Clifford Wolf <clifford@clifford.at>

--- verilog/simplebfxp.v
+++ verilog/simplebfxp.v
@@ -4,11 +4,27 @@ module simplebfxp (
 	input      [ 4:0] start, len, dest,
 	output reg [31:0] rd
 );
+	wire [ 4:0] ror_shamt = start - dest;
+	wire [31:0] ror_dout;
+
+	wire [31:0] lmask = 32'h ffff_ffff >> (32-dest-len);
+	wire [31:0] rmask = 32'h ffff_ffff << dest;
+
+	ror ror_inst (.din(rs1), .shamt(ror_shamt), .dout(ror_dout));
+
 	always @(posedge clock) begin
 		if (start+len > 32 || dest+len > 32) begin
 			rd <= 0;
 		end else begin
-			rd <= ((rs1 << (32-start-len)) >> (32-len)) << dest;
+			rd <= ror_dout & lmask & rmask;
 		end
 	end
+endmodule
+
+module ror (
+	input  [31:0] din,
+	input  [ 4:0] shamt,
+	output [31:0] dout
+);
+	assign dout = {din, din} >> shamt;
 endmodule
