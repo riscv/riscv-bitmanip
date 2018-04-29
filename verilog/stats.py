55c540f45213ae0213c1081c59f32159018481e0
Clifford Wolf <clifford@clifford.at>
2018-04-29 16:16:52 +0200

    Add largebextdep and rocketmuldiv reference cores
    Signed-off-by: Clifford Wolf <clifford@clifford.at>

--- verilog/stats.py
+++ verilog/stats.py
@@ -12,6 +12,8 @@ core_list = [
     "simplegzip",
     "simplebfxp",
     "simplebextdep",
+    "largebextdep",
+    "rocketmuldiv",
 ]
 
 core_data = dict()
@@ -51,44 +53,71 @@ for core in core_list:
                 assert 0
     core_data[core] = (ff_count, lut_count, int(gates_count))
 
-ffs_scale = 0.5 / core_data["ror"][0]
-luts_scale = 2.0 / core_data["ror"][1]
-gates_scale = 2.0 / core_data["ror"][2]
+def maketikzpicture(scale):
+    ffs_scale = scale * 0.8 / core_data["ror"][0]
+    luts_scale = scale * 2.0 / core_data["ror"][1]
+    gates_scale = scale * 2.0 / core_data["ror"][2]
 
-# print(core_data)
+    print(r"""
+    \begin{center}
+    \begin{tikzpicture}
+    """)
+
+    y = ffs_scale*core_data["ror"][0]
+    while y < 0.7:
+        print(r"\draw[gray!20] (0,%.3f) -- (%.3f,%.3f);" % (-y, 0.5 + len(core_list), -y))
+        y += ffs_scale*core_data["ror"][0]
+
+    y = luts_scale*core_data["ror"][1]
+    while y < 3.5:
+        print(r"\draw[gray!20] (0,%.3f) -- (%.3f,%.3f);" % (y, 0.5 + len(core_list), y))
+        y += luts_scale*core_data["ror"][1]
+
+    print(r"""
+        \draw[-latex] (0,0) -- (0,4);
+        \draw (-0.3,4) node[left,rotate=90] {\tiny Logic (LUTs or Gates)};
+
+        \draw[-latex] (0,0) -- (0,-1);
+        \draw (-0.3,-1) node[right,rotate=90] {\tiny FFs};
+
+        \draw[pattern=north west lines, pattern color=blue] (0.25,3.5) rectangle (0.5,3.75);
+        \draw[pattern=crosshatch dots, pattern color=green] (0.25,3.0) rectangle (0.5,3.25);
+        \draw[pattern=crosshatch, pattern color=magenta] (0.25,2.5) rectangle (0.5,2.75);
+
+        \draw (0.5,3.5 + 0.125) node[right] {\tiny ASIC Gates (ror $=$ %d)};
+        \draw (0.5,3.0 + 0.125) node[right] {\tiny FPGA 4-LUTs (ror $=$ %d)};
+        \draw (0.5,2.5 + 0.125) node[right] {\tiny D-FFs (ror $=$ %d)};
+    """ % (core_data["ror"][2], core_data["ror"][1], core_data["ror"][0]))
+
+    for idx, core in enumerate(core_list):
+        ffs, luts, gates = core_data[core]
+        print(r"\draw (%.3f,0) node[above right,rotate=90] {\tiny\tt %s};" % (0.55+idx, core))
+        print(r"\fill[white] (%.3f,0) rectangle (%.3f,%.3f);" % (0.5+idx, 0.75+idx, gates_scale*gates))
+        print(r"\fill[white] (%.3f,0) rectangle (%.3f,%.3f);" % (0.75+idx, 1.00+idx, luts_scale*luts))
+        print(r"\fill[white] (%.3f,0) rectangle (%.3f,%.3f);" % (0.5+idx, 1.0+idx, -ffs_scale*ffs))
+        print(r"\draw[pattern=north west lines, pattern color=blue] (%.3f,0) rectangle (%.3f,%.3f);" % (0.5+idx, 0.75+idx, gates_scale*gates))
+        print(r"\draw[pattern=crosshatch dots, pattern color=green] (%.3f,0) rectangle (%.3f,%.3f);" % (0.75+idx, 1.00+idx, luts_scale*luts))
+        print(r"\draw[pattern=crosshatch, pattern color=magenta] (%.3f,0) rectangle (%.3f,%.3f);" % (0.5+idx, 1.0+idx, -ffs_scale*ffs))
+
+    print(r"""
+        \draw (0,0) -- (%.3f,0);
+    \end{tikzpicture}
+    \end{center}
+    """ % (0.5 + len(core_list)))
+
 print(r"""
 \documentclass{report}
 \usepackage{tikz}
 \usetikzlibrary{patterns}
 \begin{document}
+""")
 
-\begin{center}
-\begin{tikzpicture}
-	\draw[-latex] (0,0) -- (0,4);
-    \draw (-0.3,4) node[left,rotate=90] {\tiny Logic (LUTs or Gates)};
+maketikzpicture(0.17)
 
-	\draw[-latex] (0,0) -- (0,-2);
-    \draw (-0.3,-2) node[right,rotate=90] {\tiny Flip-Flops};
-
-    \draw[pattern=north west lines, pattern color=blue] (0.25,3.5) rectangle (0.5,3.75);
-    \draw[pattern=crosshatch dots, pattern color=green] (0.25,3.0) rectangle (0.5,3.25);
-    \draw[pattern=crosshatch, pattern color=magenta] (0.25,2.5) rectangle (0.5,2.75);
-
-    \draw (0.5,3.5 + 0.125) node[right] {\tiny ASIC Gates (ror $=$ %d)};
-    \draw (0.5,3.0 + 0.125) node[right] {\tiny FPGA 4-LUTs (ror $=$ %d)};
-    \draw (0.5,2.5 + 0.125) node[right] {\tiny D-FFs (ror $=$ %d)};
-""" % (core_data["ror"][2], core_data["ror"][1], core_data["ror"][0]))
-
-for idx, core in enumerate(core_list):
-    ffs, luts, gates = core_data[core]
-    print(r"\draw (%.3f,0) node[above right,rotate=90] {\tiny\tt %s};" % (0.55+idx, core))
-    print(r"\draw[pattern=north west lines, pattern color=blue] (%.3f,0) rectangle (%.3f,%.3f);" % (0.5+idx, 0.75+idx, gates_scale*gates))
-    print(r"\draw[pattern=crosshatch dots, pattern color=green] (%.3f,0) rectangle (%.3f,%.3f);" % (0.75+idx, 1.00+idx, luts_scale*luts))
-    print(r"\draw[pattern=crosshatch, pattern color=magenta] (%.3f,0) rectangle (%.3f,%.3f);" % (0.5+idx, 1.0+idx, -ffs_scale*ffs))
+# core_list = [core for core in core_list if core != "rocketmuldiv"]
+# maketikzpicture(1.0)
 
 print(r"""
-	\draw (0,0) -- (%.3f,0);
-\end{tikzpicture}
-\end{center}
 \end{document}
-""" % (0.5 + len(core_list)))
+""")
+
