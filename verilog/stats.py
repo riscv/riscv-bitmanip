602e2b3e4ad8e514a746432ea5435e967855f003
Clifford Wolf <clifford@clifford.at>
2018-04-29 21:28:02 +0200

    Improve verilog/stats.py
    Signed-off-by: Clifford Wolf <clifford@clifford.at>

--- verilog/stats.py
+++ verilog/stats.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
-#
-# Usage:
-# yosys stats.ys
-# python3 stats.py > stats.tex && pdflatex stats.tex
 
 core_list = [
     "ror",
     "tinygrev",
-    "tinygzip",
     "simplegrev",
+    "tinygzip",
     "simplegzip",
     "simplebfxp",
     "simplebextdep",
     "smartbextdep",
+    "XBitmanip",
     "rocketmuldiv",
 ]
 
 core_data = dict()
 
 for core in core_list:
+    if core == "XBitmanip":
+        continue
     ff_count = 0
     lut_count = 0
     gates_count = 0
@@ -53,15 +52,28 @@ for core in core_list:
                 assert 0
     core_data[core] = (ff_count, lut_count, int(gates_count))
 
+ff_count = 0
+lut_count = 0
+gates_count = 0
+
+for core in ["simplegrev", "simplegzip", "smartbextdep"]:
+    ff_count += core_data[core][0]
+    lut_count += core_data[core][1]
+    gates_count += core_data[core][2]
+
+core_data["XBitmanip"] = (ff_count, lut_count, gates_count)
+
 def maketikzpicture(scale):
     ffs_scale = scale * 0.8 / core_data["ror"][0]
     luts_scale = scale * 2.0 / core_data["ror"][1]
     gates_scale = scale * 2.0 / core_data["ror"][2]
 
-    print(r"""
-    \begin{center}
-    \begin{tikzpicture}
-    """)
+    ffs_scale = 5*gates_scale
+
+    print(r"1 LUT $\hat{=}$ %.2f Gates, 1 FF $\hat{=}$ %.2f Gates \\" % (luts_scale / gates_scale, ffs_scale / gates_scale))
+
+    print(r"\begin{center}")
+    print(r"\begin{tikzpicture}")
 
     y = ffs_scale*core_data["ror"][0]
     while y < 0.7:
@@ -74,22 +86,22 @@ def maketikzpicture(scale):
         y += luts_scale*core_data["ror"][1]
 
     print(r"""
-        \fill[white] (0,2.5) rectangle (3.6,4);
+\fill[white] (0,2.5) rectangle (3.6,4);
 
-        \draw[-latex] (0,0) -- (0,4);
-        \draw (-0.2,4) node[left,rotate=90] {\tiny Logic (LUTs or Gates)};
+\draw[-latex] (0,0) -- (0,4);
+\draw (-0.2,4) node[left,rotate=90] {\tiny Logic (LUTs or Gates)};
 
-        \draw[-latex] (0,0) -- (0,-1);
-        \draw (-0.2,-1) node[right,rotate=90] {\tiny FFs};
+\draw[-latex] (0,0) -- (0,-1);
+\draw (-0.2,-1) node[right,rotate=90] {\tiny FFs};
 
-        \draw[pattern=north west lines, pattern color=blue] (0.25,3.5) rectangle (0.5,3.75);
-        \draw[pattern=crosshatch dots, pattern color=green] (0.25,3.0) rectangle (0.5,3.25);
-        \draw[pattern=crosshatch, pattern color=magenta] (0.25,2.5) rectangle (0.5,2.75);
+\draw[pattern=north west lines, pattern color=blue] (0.25,3.5) rectangle (0.5,3.75);
+\draw[pattern=crosshatch dots, pattern color=green] (0.25,3.0) rectangle (0.5,3.25);
+\draw[pattern=crosshatch, pattern color=magenta] (0.25,2.5) rectangle (0.5,2.75);
 
-        \draw (0.5,3.5 + 0.125) node[right] {\tiny ASIC Gates (ror $=$ %d)};
-        \draw (0.5,3.0 + 0.125) node[right] {\tiny FPGA 4-LUTs (ror $=$ %d)};
-        \draw (0.5,2.5 + 0.125) node[right] {\tiny D-FFs (ror $=$ %d)};
-    """ % (core_data["ror"][2], core_data["ror"][1], core_data["ror"][0]))
+\draw (0.5,3.5 + 0.125) node[right] {\tiny ASIC Gates (ror $=$ %d)};
+\draw (0.5,3.0 + 0.125) node[right] {\tiny FPGA 4-LUTs (ror $=$ %d)};
+\draw (0.5,2.5 + 0.125) node[right] {\tiny D-FFs (ror $=$ %d)};
+""" % (core_data["ror"][2], core_data["ror"][1], core_data["ror"][0]))
 
     for idx, core in enumerate(core_list):
         ffs, luts, gates = core_data[core]
@@ -101,22 +113,26 @@ def maketikzpicture(scale):
         print(r"\draw[pattern=crosshatch dots, pattern color=green] (%.3f,0) rectangle (%.3f,%.3f);" % (0.75+idx, 1.00+idx, luts_scale*luts))
         print(r"\draw[pattern=crosshatch, pattern color=magenta] (%.3f,0) rectangle (%.3f,%.3f);" % (0.5+idx, 1.0+idx, -ffs_scale*ffs))
 
-    print(r"""
-        \draw (0,0) -- (%.3f,0);
-    \end{tikzpicture}
-    \end{center}
-    """ % (0.5 + len(core_list)))
+    print(r"\draw (0,0) -- (%.3f,0);" % (0.5 + len(core_list)))
+    print(r"\end{tikzpicture}")
+    print(r"\end{center}")
 
 print(r"""
 \documentclass{report}
 \usepackage{tikz}
 \usetikzlibrary{patterns}
 \begin{document}
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
 """)
 
 maketikzpicture(0.17)
 
 print(r"""
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
 \begin{center}
 \begin{tabular}{lrrr}
 Module & \# of Gates & \# of 4-LUTs & \# of FFs \\
@@ -124,13 +140,19 @@ Module & \# of Gates & \# of 4-LUTs & \# of FFs \\
 
 for core in core_list:
     ffs, luts, gates = core_data[core]
-    if core in ["ror", "tinygrev", "simplegrev", "smartbextdep", "rocketmuldiv"]:
+    if core in ["ror", "tinygrev", "tinygzip", "simplebfxp", "simplebextdep", "XBitmanip"]:
         print(r"\hline")
-    print(r"{\tt %s} & %d & %d & %d \\" % (core, gates, luts, ffs))
+    footnote = ""
+    if core == "XBitmanip":
+        footnote = r"\footnote{{\tt simplegrev} $+$ {\tt simplegzip} $+$ {\tt smartbextdep}}"
+    print(r"{\tt %s}%s & %d & %d & %d \\" % (core, footnote, gates, luts, ffs))
 
 print(r"""
 \end{tabular}
 \end{center}
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
 """)
 
 # core_list = [core for core in core_list if core != "rocketmuldiv"]
