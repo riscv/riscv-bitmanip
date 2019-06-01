fa6d0de0b159138e8027c65c47947cb3de2119b2
Clifford Wolf <clifford@clifford.at>
2019-06-01 18:59:16 +0200

    Update rvintrin.h
    Signed-off-by: Clifford Wolf <clifford@clifford.at>

--- cproofs/rvintrin.h
+++ cproofs/rvintrin.h
@@ -1,3 +1,38 @@
+/*
+ *  RISC-V "B" extension proposal intrinsics and emulation
+ *
+ *  Copyright (C) 2019  Clifford Wolf <clifford@clifford.at>
+ *
+ *  Permission to use, copy, modify, and/or distribute this software for any
+ *  purpose with or without fee is hereby granted, provided that the above
+ *  copyright notice and this permission notice appear in all copies.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ *  ----------------------------------------------------------------------
+ *
+ *  Define RVINTRIN_EMULATE to enable emulation mode.
+ *
+ *  This header defines C inline functions with "mockup intrinsics" for
+ *  RISC-V "B" extension proposal instructions.
+ *
+ *  _rv_*(...)
+ *    RV32/64 intrinsics that operate on the "long" data type
+ *
+ *  _rv32_*(...)
+ *    RV32/64 intrinsics that operate on the "int32_t" data type
+ *
+ *  _rv64_*(...)
+ *    RV64-only intrinsics that operate on the "int64_t" data type
+ *
+ */
+
 #ifndef RVINTRIN_H
 #define RVINTRIN_H
 
