5e63e5955512a62cd3ba188e19cbff80d10f3998
Clifford Wolf <clifford@clifford.at>
2019-06-04 10:49:47 +0200

    Update patches/gcc-demo
    Signed-off-by: Clifford Wolf <clifford@clifford.at>

--- patches/gcc-demo.c
+++ patches/gcc-demo.c
@@ -1,8 +1,15 @@
+#include <rvintrin.h>
+
 int demo_asm(int val)
 {
 	int cnt = __builtin_riscv_pcntw(val);
 	__asm__ ("pcntw %0, %1" : "=r"(cnt) : "r"(val));
 	return cnt;
+}
+
+int demo_intrin(int val)
+{
+	return _rv32_pcnt(val);
 }
 
 int demo_builtin(int val)
