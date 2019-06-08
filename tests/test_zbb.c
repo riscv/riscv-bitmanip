ca323e3781547fb590b2bf66e67c24b4a40dfa3c
Clifford Wolf <clifford@clifford.at>
2019-06-08 13:17:59 +0200

    Improve spike patch, improve test_zbb
    Signed-off-by: Clifford Wolf <clifford@clifford.at>

--- tests/test_zbb.c
+++ tests/test_zbb.c
@@ -4,7 +4,69 @@ long test_zbb(uint64_t *argp, int argc)
 #define CK(_expr) do { cksum = _rv_crc32c_d(cksum ^ (_expr)); } while (0)
 
 	for (int i = 0; i < argc; i++)
-	for (int j = 0; j < argc; j++) {
+	{
+		CK(_rv32_clz(argp[i]));
+		CK(_rv32_ctz(argp[i]));
+		CK(_rv32_pcnt(argp[i]));
+
+		CK(_rv32_grev(argp[i], -1));
+		CK(_rv32_grev(argp[i], -8));
+
+		CK(_rv32_slo(argp[i], 2));
+		CK(_rv32_sro(argp[i], 3));
+		CK(_rv32_slo(argp[i], 5));
+		CK(_rv32_sro(argp[i], 7));
+
+		CK(_rv32_rol(argp[i], 2));
+		CK(_rv32_ror(argp[i], 3));
+		CK(_rv32_rol(argp[i], 5));
+		CK(_rv32_ror(argp[i], 7));
+
+		CK(_rv64_clz(argp[i]));
+		CK(_rv64_ctz(argp[i]));
+		CK(_rv64_pcnt(argp[i]));
+
+		CK(_rv64_grev(argp[i], -1));
+		CK(_rv64_grev(argp[i], -8));
+
+		CK(_rv64_slo(argp[i], 2));
+		CK(_rv64_sro(argp[i], 3));
+		CK(_rv64_slo(argp[i], 5));
+		CK(_rv64_sro(argp[i], 7));
+
+		CK(_rv64_rol(argp[i], 2));
+		CK(_rv64_ror(argp[i], 3));
+		CK(_rv64_rol(argp[i], 5));
+		CK(_rv64_ror(argp[i], 7));
+	}
+
+	for (int i = 0; i < argc; i++)
+	for (int j = 0; j < argc; j++)
+	{
+		CK(_rv32_slo(argp[i], argp[j]));
+		CK(_rv32_sro(argp[i], argp[j]));
+
+		CK(_rv32_min(argp[i], argp[j]));
+		CK(_rv32_max(argp[i], argp[j]));
+
+		CK(_rv32_minu(argp[i], argp[j]));
+		CK(_rv32_maxu(argp[i], argp[j]));
+
+		CK(_rv32_rol(argp[i], argp[j]));
+		CK(_rv32_ror(argp[i], argp[j]));
+
+		CK(_rv64_slo(argp[i], argp[j]));
+		CK(_rv64_sro(argp[i], argp[j]));
+
+		CK(_rv64_min(argp[i], argp[j]));
+		CK(_rv64_max(argp[i], argp[j]));
+
+		CK(_rv64_minu(argp[i], argp[j]));
+		CK(_rv64_maxu(argp[i], argp[j]));
+
+		CK(_rv64_rol(argp[i], argp[j]));
+		CK(_rv64_ror(argp[i], argp[j]));
+
 		CK(_rv_andn(argp[i], argp[j]));
 		CK(_rv_orn (argp[i], argp[j]));
 		CK(_rv_xnor(argp[i], argp[j]));
