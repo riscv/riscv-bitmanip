77c62bf27aca6fff7d95ce1b304f28df35e3af01
Clifford Wolf <clifford@clifford.at>
2019-06-08 13:38:58 +0200

    Improve spike patch, Add proper test_zbp
    Signed-off-by: Clifford Wolf <clifford@clifford.at>

--- tests/test_zbp.c
+++ tests/test_zbp.c
@@ -1,6 +1,78 @@
-uint64_t test_zbp(uint64_t *argp, int argc)
+long test_zbp(uint64_t *argp, int argc)
 {
-	uint64_t cksum = 123456789LL;
-	// FIXME
+	long cksum = 123456789LL;
+#define CK(_expr) do { cksum = _rv_crc32c_d(cksum ^ (_expr)); } while (0)
+
+	for (int i = 0; i < argc; i++)
+	{
+		CK(_rv64_grev(argp[i], 1));
+		CK(_rv64_grev(argp[i], 2));
+		CK(_rv64_grev(argp[i], 4));
+		CK(_rv64_grev(argp[i], 8));
+		CK(_rv64_grev(argp[i], 16));
+		CK(_rv64_grev(argp[i], 32));
+
+		CK(_rv64_grev(argp[i], -1));
+		CK(_rv64_grev(argp[i], -4));
+
+		CK(_rv64_shfl(argp[i], 1));
+		CK(_rv64_shfl(argp[i], 2));
+		CK(_rv64_shfl(argp[i], 4));
+		CK(_rv64_shfl(argp[i], 8));
+		CK(_rv64_shfl(argp[i], 16));
+
+		CK(_rv64_shfl(argp[i], -1));
+		CK(_rv64_shfl(argp[i], -4));
+
+		CK(_rv64_unshfl(argp[i], 1));
+		CK(_rv64_unshfl(argp[i], 2));
+		CK(_rv64_unshfl(argp[i], 4));
+		CK(_rv64_unshfl(argp[i], 8));
+		CK(_rv64_unshfl(argp[i], 16));
+
+		CK(_rv64_unshfl(argp[i], -1));
+		CK(_rv64_unshfl(argp[i], -4));
+
+		CK(_rv32_grev(argp[i], 1));
+		CK(_rv32_grev(argp[i], 2));
+		CK(_rv32_grev(argp[i], 4));
+		CK(_rv32_grev(argp[i], 8));
+		CK(_rv32_grev(argp[i], 16));
+		CK(_rv32_grev(argp[i], 32));
+
+		CK(_rv32_grev(argp[i], -1));
+		CK(_rv32_grev(argp[i], -4));
+
+		CK(_rv32_shfl(argp[i], 1));
+		CK(_rv32_shfl(argp[i], 2));
+		CK(_rv32_shfl(argp[i], 4));
+		CK(_rv32_shfl(argp[i], 8));
+		CK(_rv32_shfl(argp[i], 16));
+
+		CK(_rv32_shfl(argp[i], -1));
+		CK(_rv32_shfl(argp[i], -4));
+
+		CK(_rv32_unshfl(argp[i], 1));
+		CK(_rv32_unshfl(argp[i], 2));
+		CK(_rv32_unshfl(argp[i], 4));
+		CK(_rv32_unshfl(argp[i], 8));
+		CK(_rv32_unshfl(argp[i], 16));
+
+		CK(_rv32_unshfl(argp[i], -1));
+		CK(_rv32_unshfl(argp[i], -4));
+	}
+
+	for (int i = 0; i < argc; i++)
+	for (int j = 0; j < argc; j++)
+	{
+		CK(_rv64_grev(argp[i], argp[j]));
+		CK(_rv64_shfl(argp[i], argp[j]));
+		CK(_rv64_unshfl(argp[i], argp[j]));
+
+		CK(_rv32_grev(argp[i], argp[j]));
+		CK(_rv32_shfl(argp[i], argp[j]));
+		CK(_rv32_unshfl(argp[i], argp[j]));
+	}
+
 	return cksum;
 }
