935686830c98b4b3299efed51482018913e1d7be
Clifford Wolf <clifford@clifford.at>
2019-06-08 14:18:47 +0200

    Add spike bext/bdep support, Add test_zbe
    Signed-off-by: Clifford Wolf <clifford@clifford.at>

--- tests/test_zbe.c
+++ tests/test_zbe.c
@@ -1,6 +1,17 @@
-uint64_t test_zbe(uint64_t *argp, int argc)
+long test_zbe(uint64_t *argp, int argc)
 {
-	uint64_t cksum = 123456789LL;
-	// FIXME
+	long cksum = 123456789LL;
+#define CK(_expr) do { cksum = _rv_crc32c_d(cksum ^ (_expr)); } while (0)
+
+	for (int i = 0; i < argc; i++)
+	for (int j = 0; j < argc; j++)
+	{
+		CK(_rv64_bext(argp[i], argp[j]));
+		CK(_rv64_bdep(argp[i], argp[j]));
+
+		CK(_rv32_bext(argp[i], argp[j]));
+		CK(_rv32_bdep(argp[i], argp[j]));
+	}
+
 	return cksum;
 }
