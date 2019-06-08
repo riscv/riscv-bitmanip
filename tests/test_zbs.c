1c7ceef0d043e0eaad11f24f37f8a92b3c205051
Clifford Wolf <clifford@clifford.at>
2019-06-08 13:50:55 +0200

    Add spike support for SB* instructions
    Signed-off-by: Clifford Wolf <clifford@clifford.at>

--- tests/test_zbs.c
+++ tests/test_zbs.c
@@ -1,6 +1,68 @@
-uint64_t test_zbs(uint64_t *argp, int argc)
+long test_zbs(uint64_t *argp, int argc)
 {
-	uint64_t cksum = 123456789LL;
-	// FIXME
+	long cksum = 123456789LL;
+#define CK(_expr) do { cksum = _rv_crc32c_d(cksum ^ (_expr)); } while (0)
+
+	for (int i = 0; i < argc; i++)
+	{
+		CK(_rv64_sbset(argp[i], 1));
+		CK(_rv64_sbset(argp[i], 16));
+		CK(_rv64_sbset(argp[i], 24));
+		CK(_rv64_sbset(argp[i], 32));
+		CK(_rv64_sbset(argp[i], 48));
+
+		CK(_rv64_sbclr(argp[i], 1));
+		CK(_rv64_sbclr(argp[i], 16));
+		CK(_rv64_sbclr(argp[i], 24));
+		CK(_rv64_sbclr(argp[i], 32));
+		CK(_rv64_sbclr(argp[i], 48));
+
+		CK(_rv64_sbinv(argp[i], 1));
+		CK(_rv64_sbinv(argp[i], 16));
+		CK(_rv64_sbinv(argp[i], 24));
+		CK(_rv64_sbinv(argp[i], 32));
+		CK(_rv64_sbinv(argp[i], 48));
+
+		CK(_rv64_sbext(argp[i], 1));
+		CK(_rv64_sbext(argp[i], 16));
+		CK(_rv64_sbext(argp[i], 24));
+		CK(_rv64_sbext(argp[i], 32));
+		CK(_rv64_sbext(argp[i], 48));
+
+		CK(_rv32_sbset(argp[i], 1));
+		CK(_rv32_sbset(argp[i], 16));
+		CK(_rv32_sbset(argp[i], 24));
+		CK(_rv32_sbset(argp[i], 31));
+
+		CK(_rv32_sbclr(argp[i], 1));
+		CK(_rv32_sbclr(argp[i], 16));
+		CK(_rv32_sbclr(argp[i], 24));
+		CK(_rv32_sbclr(argp[i], 31));
+
+		CK(_rv32_sbinv(argp[i], 1));
+		CK(_rv32_sbinv(argp[i], 16));
+		CK(_rv32_sbinv(argp[i], 24));
+		CK(_rv32_sbinv(argp[i], 31));
+
+		CK(_rv32_sbext(argp[i], 1));
+		CK(_rv32_sbext(argp[i], 16));
+		CK(_rv32_sbext(argp[i], 24));
+		CK(_rv32_sbext(argp[i], 31));
+	}
+
+	for (int i = 0; i < argc; i++)
+	for (int j = 0; j < argc; j++)
+	{
+		CK(_rv64_sbset(argp[i], argp[j]));
+		CK(_rv64_sbclr(argp[i], argp[j]));
+		CK(_rv64_sbinv(argp[i], argp[j]));
+		CK(_rv64_sbext(argp[i], argp[j]));
+
+		CK(_rv32_sbset(argp[i], argp[j]));
+		CK(_rv32_sbclr(argp[i], argp[j]));
+		CK(_rv32_sbinv(argp[i], argp[j]));
+		CK(_rv32_sbext(argp[i], argp[j]));
+	}
+
 	return cksum;
 }
