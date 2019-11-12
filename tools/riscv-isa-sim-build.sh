54c6ec4d756fcd4ce2e8687458070199c0c45bae
Clifford Wolf <clifford@clifford.at>
2019-11-12 17:03:34 +0100

    Add --enable-misaligned to riscv-isa-sim-build.sh
    Signed-off-by: Clifford Wolf <clifford@clifford.at>

--- tools/riscv-isa-sim-build.sh
+++ tools/riscv-isa-sim-build.sh
@@ -3,6 +3,6 @@ set -ex
 rm -rf riscv-isa-sim-build
 mkdir -p riscv-isa-sim-build
 cd riscv-isa-sim-build
-../riscv-isa-sim/configure --prefix=/opt/riscv64b
+../riscv-isa-sim/configure --prefix=/opt/riscv64b --enable-misaligned
 make -j$(nproc)
 make install
